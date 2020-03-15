aefc445976aaea493601158140b491c355a38827
斯蒂芬周 <1396756487@qq.com>
2020-03-15 15:35:52 +0800

    结构搭建完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入路由
+import router from "./router/index"
+// 导入模块
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+//注册
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import VueRouter from "vue-router"
+
+
+// 注册
+Vue.use(VueRouter)
+// 实例化
+const router = new VueRouter({
+    routes: [
+        // {
+            // path: "/",
+            // component: home
+        // }
+    ]
+})
+// 输出 出去router
+export default router
